d40a286786ae14a1c9ab5401e085765e9566944d
Sethu32 <sgajana32@gmail.com>
2024-09-25 21:32:00 +0200

    updated Index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <WeatherSearch />
+    <h3>Open Sourced on <a href="https://github.com/Sethu32/Weather-React" target="_blank" rel="noreferrer">Github</a></h3>
+     
   </React.StrictMode>
 );
 
