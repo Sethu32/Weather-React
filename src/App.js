b46dd21fd0adbb95713bc1f3225e7cc41e2176ac
Sethu32 <sgajana32@gmail.com>
2024-10-10 15:49:47 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,66 +1,13 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./styles.css";
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-export default function WeatherSearch() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
-
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "a710bd8bd76400c9658ef649d9e81728";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <div className="WeatherApp">
-      <h1> Weather App </h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Enter a city.."
-          onChange={updateCity}
-        />
-        <button type="Submit">Search</button>
-      </form>
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="San Francisco" />
+      </div>
     </div>
   );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
-        <h3>Open Sourced on <a href="https://github.com/Sethu32/Weather-React" target="_blank" rel="noreferrer">Github</a></h3>
-      </div>
-    );
-  } else {
-    return form;
-  }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import WeatherSearch from './App.js';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <WeatherSearch />
-    <h3>Open Sourced on <a href="https://github.com/Sethu32/Weather-React" target="_blank" rel="noreferrer">Github</a></h3>
-     
-  </React.StrictMode>
-);
-
-reportWebVitals();
-
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
